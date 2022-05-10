bad2573bdd2f8233e8d2d6009d51e9bc97b32fed
Your Name <you@example.com>
2022-05-10 10:05:02 -0700

    changes after break

--- src/test/gitHubPractice.java
+++ src/test/gitHubPractice.java
@@ -3,8 +3,11 @@ package test;
 public class gitHubPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// TODO Auto-generated method stub
+int a =10;
+int b = 30;
+int c = a+b;
+System.out.println(a+b);
 	}
 
 }

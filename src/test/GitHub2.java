3684a2084d15caf8ea0d544eb13d2033fd82ed5b
Your Name <you@example.com>
2022-05-10 10:21:03 -0700

    new gitHub class

---
+++ src/test/GitHub2.java
@@ -0,0 +1,12 @@
+package test;
+
+public class GitHub2 {
+
+	public static void main(String[] args) {
+		
+String name =("ahmad, mahmod");
+String moble = ("Iphon, Samsung, Nokia");
+System.out.println(name +"  "+moble);
+	}
+
+}

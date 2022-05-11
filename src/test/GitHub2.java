db2cfd80c0d0aa6cc11ce582e29d3067183fbdb3
Your Name <you@example.com>
2022-05-11 12:07:47 -0700

    adding sharks class

--- src/test/GitHub2.java
+++ src/test/GitHub2.java
@@ -4,9 +4,10 @@ public class GitHub2 {
 
 	public static void main(String[] args) {
 		
-String name =("ahmad, mahmod");
-String moble = ("Iphon, Samsung, Nokia");
+String name =("ahmad, mahmod, john");
+String moble = ("Iphon, Samsung, Nokia, Huwawi");
 System.out.println(name +"  "+moble);
+System.out.println();
 	}
 
 }
